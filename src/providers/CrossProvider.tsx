aa671b289390e4919d7f5777f0a17d7599c57870
Jeton Kukalaj <jeton.kukalaj@crispybacon.it>
2022-02-12 12:51:05 +0100

    add `GestureHandlerRootView` and fix import

--- src/providers/CrossProvider.tsx
+++ src/providers/CrossProvider.tsx
@@ -3,20 +3,23 @@ import { DripsyProvider } from 'dripsy';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { PortalProvider } from '@gorhom/portal';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import theme from '../configs';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { theme } from '../configs';
 
 type CrossProviderProps = {
   children: ReactNode;
 };
 
 const CrossProvider = ({ children }: CrossProviderProps) => (
-  <DripsyProvider theme={theme}>
-    <BottomSheetModalProvider>
-      <PortalProvider>
-        <SafeAreaProvider>{children}</SafeAreaProvider>
-      </PortalProvider>
-    </BottomSheetModalProvider>
-  </DripsyProvider>
+  <GestureHandlerRootView style={{ flex: 1 }}>
+    <DripsyProvider theme={theme}>
+      <BottomSheetModalProvider>
+        <PortalProvider>
+          <SafeAreaProvider>{children}</SafeAreaProvider>
+        </PortalProvider>
+      </BottomSheetModalProvider>
+    </DripsyProvider>
+  </GestureHandlerRootView>
 );
 
 export default CrossProvider;
